999931d36b665a5fb035cacdfd966c6778c7379a
OlessyaPriora <Olessya_29_03_91@mail.ru>
2024-07-28 17:48:45 +0300

    Created model Category

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_migrate import Migrate
+
+from app.models import db
+from app.routes.questions import questions_bp
+from app.routes.responses import responses_bp
+from config import DevelopmentConfig
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(DevelopmentConfig)
+    db.init_app(app)
+    migrate = Migrate()
+    migrate.init_app(app, db)
+    app.register_blueprint(questions_bp)
+    app.register_blueprint(responses_bp)
+    return app
